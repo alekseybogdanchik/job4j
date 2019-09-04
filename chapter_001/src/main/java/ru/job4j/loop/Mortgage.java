b2dfb90fd7df2d013977d8673aa9c145affb520a
alekseybogdanchik <abp163@mail.ru>
2019-09-04 22:52:12 +0400

    5.6. Ипотека.[#165166]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        int result = amount;
+        int yearPays = monthly*12;
+        while (result>0) {
+            year++;
+            result = (int) ((result+(result*(percent/100)))-yearPays);
+           }
+        return year;
+    }
+}
