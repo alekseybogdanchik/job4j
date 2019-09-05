86f64dd91610811f009e2f5f53d5dcb2ff6d998e
alekseybogdanchik <abp163@mail.ru>
2019-09-05 09:45:17 +0400

    5.5. Простые числа.[#165165]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,15 +3,16 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int num) {
         boolean prime = false;
-        if (num % num == 0 && num % 1 == 0) {
-            prime = true;
-        }
+
         for (int index = 2; index < num; index++) {
-        if (num % index == 0) {
-            prime = false;
-            break;
+            if (num % index == 0) {
+                prime = false;
+                break;
+            } else {
+                prime = true;
             }
         }
         return prime;
     }
 }
+
--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,9 +4,7 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int num = 2; num <= finish; num++) {
-            if (num % num == 0 && num % 1 == 0) {
-                count ++;
-            }
+            count ++;
             for (int i = 2; i < num; i++) {
                 if (num % i == 0) {
                     count--;
@@ -16,5 +14,4 @@ public class PrimeNumber {
         }
         return count;
     }
-
 }
