6845dd9455b5b9edbf5ee5d9376d98136d717cb6
alekseybogdanchik <abp163@mail.ru>
2019-09-07 17:35:16 +0400

    5.7. JavaDoc. Code convention[#165162]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,55 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс Calculator выполняет арифметические действия
+ * @author Bogdanchik
+ * @since 07.09.19
+ */
+
 public class Calculator {
+    /**
+     * Сложение аргументов и вывод строки с выполняемым действием
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
-
+    /**
+     * Деление аргументов и вывод строки с выполняемым действием
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /**
+     * Умножение аргументов и вывод строки с выполняемым действием
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply (double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Вычитание аргументов и вывод строки с выполняемым действием
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtrack  (double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
+
+    /**
+     * Вызывает методы и передает аргументы
+     * @param args
+     */
 
         public static void main(String[] args) {
         add(1, 1);
