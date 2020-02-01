31b12b459502c4bb357b5c84bfe123ac70fccced
alekseybogdanchik <abp163@mail.ru>
2020-02-01 17:23:13 +0400

    Банковские переводы.[#165233]

---
+++ chapter_002/src/main/java/ru/job4j/bank/Account.java
@@ -0,0 +1,56 @@
+package ru.job4j.bank;
+
+import java.util.Objects;
+
+public class Account {
+    private String requisite;
+    private double balance;
+
+    public Account(String requisite, double balance) {
+        this.requisite = requisite;
+        this.balance = balance;
+    }
+
+    public String getRequisite() {
+        return requisite;
+    }
+
+    public void setRequisite(String requisite) {
+        this.requisite = requisite;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    boolean transfer(Account destination, double amount) {
+        boolean rsl = false;
+        if (this.getBalance() >= amount) {
+            this.setBalance(this.getBalance() - amount);
+            destination.setBalance(destination.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Account account = (Account) o;
+        return Objects.equals(requisite, account.requisite);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requisite);
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,62 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> currentAccounts = users.get(user);
+            boolean alreadyHave = currentAccounts.contains(account);
+            if (!alreadyHave) {
+                currentAccounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User rsl = null;
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                rsl = user;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account find = null;
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> currentAccounts = users.get(user);
+            for (Account account : currentAccounts) {
+                if (account.getRequisite().equals(requisite)) {
+                    find = account;
+                    break;
+                }
+            }
+        }
+        return find;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account src = findByRequisite(srcPassport, srcRequisite);
+        Account dest = findByRequisite(destPassport, destRequisite);
+        if (src != null && dest != null) {
+            rsl = src.transfer(dest, amount);
+        }
+        return rsl;
+    }
+}
