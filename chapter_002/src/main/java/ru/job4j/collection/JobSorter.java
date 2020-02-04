6f1b8f05f029576a6bf3d724323e15691fb2239c
alekseybogdanchik <abp163@mail.ru>
2020-02-04 15:20:58 +0400

    1. Организовать сортировку User[#165228]

--- chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
+++ chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
@@ -1,11 +1,14 @@
 package ru.job4j.collection;
 
+import ru.job4j.sort.JobDecrByPriority;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class JobSorter {
     public static void main(String[] args) {
+/*
         List<Job> jobs = Arrays.asList(
                 new Job("Fix bugs", 4),
                 new Job("Impl task", 2),
@@ -16,5 +19,15 @@ public class JobSorter {
         System.out.println(jobs);
         Collections.sort(jobs, new SortByNameJob());
         System.out.println(jobs);
+*/
+
+        List<Job> jobsToCombineSort = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        Collections.sort(jobsToCombineSort, new JobDecrByName().thenComparing(new JobDecrByPriority()));
+        System.out.println(jobsToCombineSort);
     }
 }
